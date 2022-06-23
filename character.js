db45b5d733e6fa647840dc3273ac84b2ffa8fe44
Rami Elsayed <ramielsayed123@outlook.com>
2022-06-23 15:35:17 +0100

    built all

--- character.js
+++ character.js
@@ -5,21 +5,33 @@ class Character {
         this.hitPoints = hitPoints;
     }
 
-    printStats();
-    isAlive();
-    attack();
+    printStats(){
+        console.log(`\n\n Player Name: ${this.name}`);
+        console.log(`Player Strength: ${this.strength}`);
+        console.log(`Player HitPoints: ${this.hitPoints}\n\n`);
+    };
+    isAlive() {
+        return this.hitPoints >= 0
+    };
+
+    attack(opponent) {
+        opponent.hitPoints -= this.strength;
+    };
 }
+
 const startGame = () => {
     let isPlayerOne = true;
-    const player1 = new Character('Rami', 50, 100);
-    const player2 = new Character('Ben', 20, 90);
+    const player1 = new Character("Rami", 50, 100);
+    const player2 = new Character("Ben", 20, 90);
+
+    player1.printStats();
+    player2.printStats();
     
 const playGame = () => {
     if (!player1.isAlive() || !player2.isAlive()) {
         clearInterval(gameInterval);
         console.log("GAME OVER");
-    }
-    if (isPlayerOne) {
+    }else if (isPlayerOne) {
         player1.attack(player2);
 
         player2.printStats();
@@ -28,7 +40,11 @@ const playGame = () => {
         
         player1.printStats();
     }
-}
+
+    isPlayerOne = !isPlayerOne;
+};
 
 const gameInterval = setInterval(playGame, 2000);
-}
+};
+
+startGame();
